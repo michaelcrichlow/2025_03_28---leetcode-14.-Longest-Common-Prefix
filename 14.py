17d8ef70742e3992837b9c813a5588a14e4a6ac0
Michael Crichlow <mikecrichlow@gmail.com>
2025-03-28 13:36:35 -0700

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,42 @@
+# Oct 23, 2024
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+    #def longestCommonPrefix(strs: list[str]) -> str:
+        #print("IN FUNCTION")
+        # guardian (no guardian for now)
+
+        # function to use later
+        def check_values(s: str, l: list[str]):
+            #print("IN VALUES FUNCTION")
+            match = True
+            for val in l:
+                if not val.startswith(s):
+                    match = False
+
+                if match == False:
+                    break
+
+            return match
+
+        # catch trivial cases
+        if len(strs) == 1:
+            return strs[0]
+
+        # initialize varaibles
+        return_string = ""
+        check_val = ""
+        local_bool = False
+        # for i, val in enumerate(strs):
+        for i in range(200):
+            if i + 1 < len(strs[0]) + 1:
+                check_val = strs[0][:i+1]
+
+            local_bool = check_values(check_val, strs)
+            if local_bool == True:
+                return_string = check_val
+            else:
+                break
+
+        #print("return_string: ", return_string)
+        return return_string
+        
